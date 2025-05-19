a1f9fa330e35e5a9bda44b76a8759a76672bc7b6
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2025-05-19 16:45:33 -0600

    reestructurar el proyecto

--- cendibiblioteca/biblioteca/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Alumno, Especialidad, Editorial, Autor, Libro, Prestamo
-
-admin.site.register(Alumno)
-admin.site.register(Especialidad)
-admin.site.register(Editorial)
-admin.site.register(Autor)
-admin.site.register(Libro)
-admin.site.register(Prestamo)
