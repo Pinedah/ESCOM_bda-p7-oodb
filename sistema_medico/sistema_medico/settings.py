d9bb7eea6d48eb9c2670123c088f7d0ce243c03a
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2025-05-19 16:47:09 -0600

    preparar variables para docker

--- sistema_medico/sistema_medico/settings.py
+++ sistema_medico/sistema_medico/settings.py
@@ -19,13 +19,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x5*fam1pjjj%zr(yt@e(gx(pe+32ep^pk(gr*!!9gg*%s5vxr)'
+#SECRET_KEY = 'django-insecure-x5*fam1pjjj%zr(yt@e(gx(pe+32ep^pk(gr*!!9gg*%s5vxr)'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-x5*fam1pjjj%zr(yt@e(gx(pe+32ep^pk(gr*!!9gg*%s5vxr)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
